3234368ecef1fd7a2defce3eb5460fefbf1da325
Artem Malieiev <artem.malieiev@gmail.com>
2024-01-30 10:30:45 +0200

    removed .webpack folder

--- src/http/get-users.ts
+++ src/http/get-users.ts
@@ -1,12 +1,12 @@
-const DynamoDB = require("@aws-sdk/client-dynamodb").DynamoDB;
-const DynamoLib = require("@aws-sdk/lib-dynamodb");
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDB({});
-const documentClient = DynamoLib.DynamoDBDocumentClient.from(client);
+const documentClient = DynamoDBDocumentClient.from(client);
 
 exports.handler = async (event) => {
   const result = await documentClient.send(
-    new DynamoLib.ScanCommand({
+    new ScanCommand({
       TableName: "users",
     })
   );
